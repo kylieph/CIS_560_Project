d0a74162bf0342a4902072cfbbcacfe38763996d
kaitlynpritchard7 <kgpritchard4@gmail.com>
2025-04-25 16:36:10 -0500

    Adding items to customer form

--- Jewelry_Project/Jewelry_Project/CustomerForm.Designer.cs
+++ Jewelry_Project/Jewelry_Project/CustomerForm.Designer.cs
@@ -32,7 +32,10 @@
             this.metalComboBox = new System.Windows.Forms.ComboBox();
             this.categoryComboBox = new System.Windows.Forms.ComboBox();
             this.newReleasesButton = new System.Windows.Forms.Button();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.itemsFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // profileButton
@@ -49,7 +52,7 @@
             // metalComboBox
             // 
             this.metalComboBox.FormattingEnabled = true;
-            this.metalComboBox.Location = new System.Drawing.Point(52, 254);
+            this.metalComboBox.Location = new System.Drawing.Point(47, 299);
             this.metalComboBox.Name = "metalComboBox";
             this.metalComboBox.Size = new System.Drawing.Size(153, 28);
             this.metalComboBox.TabIndex = 1;
@@ -57,7 +60,7 @@
             // categoryComboBox
             // 
             this.categoryComboBox.FormattingEnabled = true;
-            this.categoryComboBox.Location = new System.Drawing.Point(52, 356);
+            this.categoryComboBox.Location = new System.Drawing.Point(47, 392);
             this.categoryComboBox.Name = "categoryComboBox";
             this.categoryComboBox.Size = new System.Drawing.Size(153, 28);
             this.categoryComboBox.TabIndex = 2;
@@ -65,7 +68,7 @@
             // newReleasesButton
             // 
             this.newReleasesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.newReleasesButton.Location = new System.Drawing.Point(52, 155);
+            this.newReleasesButton.Location = new System.Drawing.Point(47, 200);
             this.newReleasesButton.Name = "newReleasesButton";
             this.newReleasesButton.Size = new System.Drawing.Size(153, 43);
             this.newReleasesButton.TabIndex = 3;
@@ -73,19 +76,53 @@
             this.newReleasesButton.UseVisualStyleBackColor = true;
             this.newReleasesButton.Click += new System.EventHandler(this.newReleasesButton_Click);
             // 
-            // flowLayoutPanel1
+            // itemsFlowLayoutPanel
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(256, 110);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(513, 312);
-            this.flowLayoutPanel1.TabIndex = 4;
+            this.itemsFlowLayoutPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.itemsFlowLayoutPanel.Location = new System.Drawing.Point(256, 110);
+            this.itemsFlowLayoutPanel.Name = "itemsFlowLayoutPanel";
+            this.itemsFlowLayoutPanel.Size = new System.Drawing.Size(513, 312);
+            this.itemsFlowLayoutPanel.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(31, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Filter:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label2.Location = new System.Drawing.Point(42, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Metal:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label3.Location = new System.Drawing.Point(43, 349);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 22);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Category:";
             // 
             // CustomerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.itemsFlowLayoutPanel);
             this.Controls.Add(this.newReleasesButton);
             this.Controls.Add(this.categoryComboBox);
             this.Controls.Add(this.metalComboBox);
@@ -94,6 +131,7 @@
             this.Text = "CustomerForm";
             this.Load += new System.EventHandler(this.CustomerForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -103,6 +141,9 @@
         private System.Windows.Forms.ComboBox metalComboBox;
         private System.Windows.Forms.ComboBox categoryComboBox;
         private System.Windows.Forms.Button newReleasesButton;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel itemsFlowLayoutPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Jewelry_Project/Jewelry_Project/CustomerForm.cs
+++ Jewelry_Project/Jewelry_Project/CustomerForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -26,7 +27,7 @@ namespace Jewelry_Project
 
         private void newReleasesButton_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void CustomerForm_Load(object sender, EventArgs e)
@@ -49,13 +50,50 @@ namespace Jewelry_Project
                 }
                 using (SqlCommand cmd = new SqlCommand("Store.GetAllCategoryTypes", conn))
                 {
-                    cmd.CommandType =CommandType.StoredProcedure;
+                    cmd.CommandType = CommandType.StoredProcedure;
                     using (SqlDataReader reader = cmd.ExecuteReader())
                     {
                         while (reader.Read())
                         {
                             string categoryName = reader["CategoryName"].ToString();
                             categoryComboBox.Items.Add(categoryName);
+                        }
+                    }
+                }
+                using (SqlCommand cmd = new SqlCommand("Store.GetAllProducts", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string itemName = reader["ItemName"].ToString();
+                            string description = reader["Description"].ToString();
+                            decimal itemPrice = (decimal)reader["ItemPrice"];
+
+                            Panel itemPanel = new Panel()
+                            {
+                                Width = 50,
+                                Height = 50,
+                                Margin = new Padding(3)
+                            };
+
+                            Label nameLabel = new Label { Text = itemName, AutoSize = true };
+                            Label priceLabel = new Label { Text = "Price: $" + itemPrice.ToString("F2"), AutoSize = true };
+                            Label descLabel = new Label { Text = description, AutoSize = true };
+
+                            itemPanel.Controls.Add(nameLabel);
+                            itemPanel.Controls.Add(priceLabel);
+                            itemPanel.Controls.Add(descLabel);
+
+                            // Stack labels vertically
+                            /*
+                            nameLabel.Location = new Point(5, 5);
+                            priceLabel.Location = new Point(5, 25);
+                            descLabel.Location = new Point(5, 45);
+                            */
+
+                            itemsFlowLayoutPanel.Controls.Add(itemPanel);
                         }
                     }
                 }
